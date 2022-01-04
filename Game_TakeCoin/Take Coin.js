77ce8212fa758f343b1c3c8a74db7e087ef75169
LunarLunar <archsean2002@yahoo.com.hk>
2022-01-04 11:39:35 +0800

    1139

--- Game_TakeCoin/Take Coin.js
+++ Game_TakeCoin/Take Coin.js
@@ -4,7 +4,7 @@ var p; // shortcut to reference prototypes
 var lib={};var ss={};var img={};
 lib.ssMetadata = [
 		{name:"Take Coin_atlas_", frames: [[0,0,1080,1920]]},
-		{name:"Take Coin_atlas_2", frames: [[0,0,1080,1920],[1082,962,540,960],[1082,0,540,960]]},
+		{name:"Take Coin_atlas_2", frames: [[0,0,1080,1920],[1082,0,540,960],[1082,962,540,960]]},
 		{name:"Take Coin_atlas_3", frames: [[443,374,282,63],[0,0,635,254],[0,374,441,145],[759,92,224,64],[759,0,208,90],[443,439,342,42],[727,374,120,21],[727,397,26,26],[0,256,861,116],[969,0,46,46],[863,361,96,104],[863,247,144,112],[637,194,381,51],[637,0,120,192],[985,132,16,48],[985,90,24,40],[969,48,40,40]]}
 ];
 
@@ -56,7 +56,7 @@ lib.ssMetadata = [
 
 
 (lib.CachedTexturedBitmap_3 = function() {
-	this.initialize(ss["Take Coin_atlas_"]);
+	this.initialize(ss["Take Coin_atlas_2"]);
 	this.gotoAndStop(0);
 }).prototype = p = new cjs.Sprite();
 
@@ -77,7 +77,7 @@ lib.ssMetadata = [
 
 
 (lib.CachedTexturedBitmap_6 = function() {
-	this.initialize(ss["Take Coin_atlas_2"]);
+	this.initialize(ss["Take Coin_atlas_"]);
 	this.gotoAndStop(0);
 }).prototype = p = new cjs.Sprite();
 
@@ -957,9 +957,9 @@ lib.properties = {
 	color: "#FFFFFF",
 	opacity: 1.00,
 	manifest: [
-		{src:"images/Take Coin_atlas_.png?1641227839918", id:"Take Coin_atlas_"},
-		{src:"images/Take Coin_atlas_2.png?1641227839918", id:"Take Coin_atlas_2"},
-		{src:"images/Take Coin_atlas_3.png?1641227839919", id:"Take Coin_atlas_3"}
+		{src:"images/Take Coin_atlas_.png?1641267404660", id:"Take Coin_atlas_"},
+		{src:"images/Take Coin_atlas_2.png?1641267404660", id:"Take Coin_atlas_2"},
+		{src:"images/Take Coin_atlas_3.png?1641267404660", id:"Take Coin_atlas_3"}
 	],
 	preloads: []
 };
@@ -1018,8 +1018,8 @@ an.getComposition = function(id) {
 
 an.makeResponsive = function(isResp, respDim, isScale, scaleType, domContainers) {		
 	var lastW, lastH, lastS=1;		
-	window.addEventListener('resize', resizeCanvas);		
-	resizeCanvas();		
+	//window.addEventListener('resize', resizeCanvas);		
+	//resizeCanvas();		
 	function resizeCanvas() {			
 		var w = lib.properties.width, h = lib.properties.height;			
 		var iw = window.innerWidth, ih=window.innerHeight;			
